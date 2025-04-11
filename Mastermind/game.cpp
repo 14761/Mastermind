8056a0a01d7665384a4e91da3569ce8a45db7f78
14761 <14761@ait.nsw.edu.au>
2025-04-11 16:06:53 +1000

    game.cpp comments added

--- Mastermind/game.cpp
+++ Mastermind/game.cpp
@@ -4,20 +4,21 @@
 
 game::game(bool isRepeat)
 {
+    // Initialize all the following variables
     round = 0;
     guess = std::string(32, '-');
     correctCheckList = std::string(8, '-');
     wrongCheckList = std::string(8, '-');
     this->isRepeat = isRepeat;
-    startTime = std::chrono::system_clock::now();
+    startTime = std::chrono::system_clock::now(); // creates a timestamp
 
-    std::string tempCode;
+    std::string tempCode; // temporary secret code
     char c; // c will save the value of the random letter
     for (int i = 0; i < codeLength; i++) // Iterate 4 times (length of the code)
     {
         std::random_device rd; // obtain a random number from hardware
         std::mt19937 gen(rd()); // seed the generator
-        if (!isRepeat) // check if the game allows to repeat letters
+        if (!isRepeat) // check if the game allows to repeat lettersd
         {
             do // iterate until the letter generated is not repeated in the code
             {
@@ -25,15 +26,15 @@ game::game(bool isRepeat)
                 c = dist(gen); // generate the number (letter)
             } while (std::find(tempCode.begin(), tempCode.end(), c) != tempCode.end());
 
-            tempCode += c; // saves the letter in the code
+            tempCode += c; // saves the letter in the temporary secret code
         }
         else // this runs if the game allows to repeat the letters in the code
         {
             std::uniform_int_distribution<int> dist('A', 'F'); // define the range of the random number (letter) to generate
             c = dist(gen); // generate the number (letter)
-            tempCode += c; // saves the letter in the code
+            tempCode += c; // saves the letter in the temporary secret code
         }
-        secretCode += c;
+        secretCode += c; // saves the letter in the secret code 
     }
 
 }
@@ -65,29 +66,30 @@ void game::displayCode()
 
 void game::checkGuess()
 {
-    int correct = 0, wrong = 0;
-    bool tempGuess[4];
-    std::fill(std::begin(tempGuess), std::end(tempGuess), false);
+    int correct = 0, wrong = 0; // variables to count the corrects' and wrongs'
+    bool tempGuess[4]; // variable to avoid overcount the guess letters when checking
+    std::fill(std::begin(tempGuess), std::end(tempGuess), false); // initialize the variable with false values
     for (int i = 0; i < codeLength; i++) // iterator on secret code
     {
         for (int j = (round * codeLength); j < (round * codeLength + codeLength); j++) // iterator on guess
         {
-            if (tempGuess[j - (round * codeLength)])
+            if (tempGuess[j - (round * codeLength)]) // checks if the guess letter has already matched a secret code letter
             {
                 continue;
             }
 
-            if (guess[j] == secretCode[i])
+            if (guess[j] == secretCode[i]) // checks if the guess letter is the same as the code letter
             {
-                if (i == j - (round * codeLength))
+                if (i == j - (round * codeLength)) // checks if the position of the guess letter is the same as code letter
                 {
                     correct++;
-                    tempGuess[j - (round * codeLength)] = true;
+                    tempGuess[j - (round * codeLength)] = true; // the guess letter has matched a code letter
                     break;
                 }
                 else
                 {
                     wrong++;
+                    tempGuess[j - (round * codeLength)] = true;
                     break;
                 }
                 
@@ -106,7 +108,7 @@ void game::inputGuess()
     {
         std::cout << "Input your guess: ";
         std::cin >> guessTemp;
-        transform(guessTemp.begin(), guessTemp.end(), guessTemp.begin(), ::toupper);
+        transform(guessTemp.begin(), guessTemp.end(), guessTemp.begin(), ::toupper); // converts the input to upper
         std::cout << std::endl;
         if (guessTemp.length() != codeLength)
         {
